8b86ca09ea337c068d4848b1ae77fd957a2d40de
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2012-10-13 02:22:44 -0400

    Update README

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , util = require('util')
 
 var app = module.exports = express.createServer();
 
@@ -25,11 +26,11 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
@@ -37,4 +38,4 @@ app.configure('production', function(){
 app.get('/', routes.index);
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings);
